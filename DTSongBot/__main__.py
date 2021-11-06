062d134a2213bfbab5ce0e322dcdb178cdef875a
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-11-06 13:39:42 +0530

    Update __main__.py

--- DTSongBot/__main__.py
+++ DTSongBot/__main__.py
@@ -19,7 +19,7 @@ pm_start_text = """
 """
 
 about_photo= """
-Test Text
+Telegraph link where the logo of Programming Boy Corporation © is uploaded 😋 https://telegra.ph/file/3466040774f3af8624948.jp\🍀 We Are Programming Boy Corporation ©  
 """
 
 
