f76b0c3a937a82e244391312c7425dbb1c0179d4
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-11-06 11:54:07 +0530

    Update __main__.py

--- DTSongBot/__main__.py
+++ DTSongBot/__main__.py
@@ -10,18 +10,18 @@ from DTSongBot import LOGGER
 pm_start_text = """
 🙆‍♀️ Hey [{}](tg://user?id={}),\n🌷 I'm 🎧 Song Downloader bot  🎵
 🙋‍♂️ Made by Programming Boy Corporation ©️\n<b>
-🥰 My command👇</b>
+🥰 My commands 👇</b>
 🍀/song : Download songs via Youtube
 🙋‍♂️/about : About Bot
 ☘️ 𝘿𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙧 @HASINDU_HIMASARA\n
 🍀 Stay Safe & Good Luck 🍀\n\n
-◇───────────────◇\n\n◇Programming Boy Corporation ©️\n◇───────────────◇
+◇───────────────◇\n\n Programming Boy Corporation ©️\n\n◇───────────────◇
 """
 
 help_text = """
 My commands👇
-- /song <song name>:👀 Download songs via Youtube 👀
--/about :✌ About Bot ✌
+- /song <song name>: 👀 Download songs via Youtube 👀
+-/about : ✌ About Bot ✌
 """
 
 @app.on_message(filters.command("start"))
