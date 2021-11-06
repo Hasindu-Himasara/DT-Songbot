8d37ab4cbf18c092f87134476c754ea87e8b7d8f
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-11-06 14:24:14 +0530

    Update songs.py

--- DTSongBot/plugins/songs.py
+++ DTSongBot/plugins/songs.py
@@ -52,7 +52,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '🎵 Uploaded By @Song_LK_Programming_Boy_Bot 🙋‍♂️\n☘ Join @TECH_WIDE_GROUP\n\n<b>🍀 Thanks For Useing Song Downloader bot ✌</b>'
+        rep = '🎵 Uploaded By @Song_LK_Programming_Boy_Bot 🙋‍♂️\n☘ Join @TECH_WIDE_GROUP\n\n🍀 Thanks For Useing Song Downloader bot ✌\n🙋‍♂️ Stay Safe 🙋‍♂️'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
