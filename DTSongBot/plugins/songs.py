2b666ae322497822e57e3ead83b002f61325e1b9
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-11-06 14:59:48 +0530

    Update songs.py

--- DTSongBot/plugins/songs.py
+++ DTSongBot/plugins/songs.py
@@ -46,7 +46,7 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit('☘ Downloading Song Please Wait...')
+    m.edit('☘️ Uploading to Telegram...')
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
